f88f87d5db351014c18e9dd409cc0a8027a9cb5b
Sangram Thapa <thapasangram64@gmail.com>
2025-03-09 12:13:48 +0545

    added

--- src/pages/home/Slug.jsx
+++ src/pages/home/Slug.jsx
@@ -1,36 +1,5 @@
-import React from "react";
-import data from "../../assets/data";
-import { useParams } from "react-router-dom";
-
 function Slug() {
-  const { id } = useParams();
-  const place = data.find((item) => item.id.toString() === id);
-
-  if (!place) {
-    return <h2 className="text-center text-2xl font-bold">Place Not Found</h2>;
-  }
-
-  return (
-    <div className="max-w-6xl mx-auto p-5">
-      <section className="h-[90px] lg:h-[80px]"></section>
-      <h1 className="text-4xl font-bold text-center mb-5">{place.title}</h1>
-      <img
-        src={place.image}
-        alt={place.title}
-        className="w-full h-[50vh] object-cover rounded-lg shadow-lg"
-      />
-      <p className="mt-5 text-lg">{place.description}</p>
-
-      <div className="flex justify-center mt-5">
-        <a
-          href="/"
-          className="py-2 px-6 bg-[#2ba1a1] text-white font-bold rounded-lg hover:bg-[#1e9596]"
-        >
-          Back to Home
-        </a>
-      </div>
-    </div>
-  );
+  return <div className="p-50">Slug</div>;
 }
 
 export default Slug;
